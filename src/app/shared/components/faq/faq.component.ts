34af317047212fcfd966dab20d8e752e1d4ececb
Rodo_VM <rodolfovmarcos@gmail.com>
2025-02-18 10:22:54 -0500

    design responsive

--- src/app/shared/components/faq/faq.component.ts
+++ src/app/shared/components/faq/faq.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.scss'
 })
